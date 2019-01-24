a83c69c03606bd1901db2db5be716483f9b1776b
ParkKyungwon <46917458+Park-Kyungwon@users.noreply.github.com>
2019-01-24 11:15:41 +0900

    Add files via upload

--- js/basicCalc.js
+++ js/basicCalc.js
@@ -2,58 +2,87 @@ var view = 0;
 var flag = 0;
 var temp = 0;
 
-function btnNumber(num){
-	view *= 10;
-	view += num;
-	$("#message").text(view);
+var pointFlag = false;
+
+function btnNumber(num) {
+	if (!pointFlag) {
+		if (Number(view) == 0 && num == 0) {
+
+		} else if (Number(view) == 0 && num != 0) {
+			view = num;
+		} else if (Number(view) != 0) {
+			view = view + "" + num;
+		}
+
+		$("#message").text(view);
+
+	} else {
+		view = view + "" + num;
+		
+		$("#message").text(view);
+	}
+
 }
 
-function btnClear(){
+function btnClear()	{
 	view = 0;
 	$("#message").text(0);
+
 }
 
 function btnPlus(){
 	temp = view;
 	flag = 1;
-	view = 0;
+	view = 0;		
+	pointFlag = false;
 }
 
 function btnMinus(){
 	temp = view;
 	flag = 2;
-	view = 0;
+	view = 0;		
+	pointFlag = false;
 }
 
 function btnMulti(){
 	temp = view;
 	flag = 3;
-	view = 0;
+	view = 0;		
+	pointFlag = false;
 }
 
 function btnDivide(){
 	temp = view;
 	flag = 4;
 	view = 0;
+	pointFlag = false;		
+
 }
 
-function btnEqual(){
-	if (flag == 1)	{
-		temp += view;
-		
-	} else if (flag == 2)	{
-		temp = temp - view;
-		
-	} else if (flag == 3)	{
-		temp = temp * view;
-		
-	} else if (flag == 4)	{
-		temp = temp / view;
-
-	} else {
-
+function btnEqual() {
+	if (flag == 1) {
+		temp = Number(temp) + Number(view);
+	} else if (flag == 2) {
+		temp = Number(temp) - Number(view);
+	} else if (flag == 3) {
+		temp = Number(temp) * Number(view);
+	} else if (flag == 4) {
+		temp = Number(temp) / Number(view);
 	}
 
 	$("#message").text(temp);
 
+	pointFlag = false;
+	view = 0;		
+
+}
+
+function btnBackspace() {
+
+}
+
+function btnPoint() {
+	pointFlag = true;
+	view += ".";
+	$("#message").text(view);
 }
