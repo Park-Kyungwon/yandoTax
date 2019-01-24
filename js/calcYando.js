2e4bdff834292a02c9f9899d1d4152bd52786df5
ParkKyungwon <46917458+Park-Kyungwon@users.noreply.github.com>
2019-01-24 13:14:26 +0900

    Add files via upload

--- js/calcYando.js
+++ js/calcYando.js
@@ -40,19 +40,57 @@ function calcYandoTax(){
 
 function calcAcqTax(){
 	//취득세구하기
-	acqVal = $("#acqVal").val();
+	acqVal = $("#acqTaxAcqVal").val();
 	
 	var ACQ_TAX_AMT_FST = 600000000;
 	var ACQ_TAX_AMT_SND = 900000000; 
 		
-	var ACQ_TAX_RT_FST = 1;
-	var ACQ_TAX_RT_SND = 2;
-	var ACQ_TAX_RT_THD = 3;
+	var ACQ_TAX_RT_GEN = 0.01;
+	var ACQ_TAX_RT_FST = 0.02;
+	var ACQ_TAX_RT_SND = 0.03;
 	
-	var FARM_TAX_RT = 10;
+	var FARM_TAX_RT = 0.1;
 	
-	if(acqVal > ACQ_TAX_SND){
+	var acqTax = 0;
+	var farmTaxAmt = 0;
+	var totTaxAmt = 0;
+	var acqRt = 0;
+	
+	if(acqVal > ACQ_TAX_AMT_SND){
+		acqTax = acqVal * ACQ_TAX_RT_SND;
+		acqRt = ACQ_TAX_RT_SND * 100;
 		
+	} if(acqVal > ACQ_TAX_AMT_FST){
+		acqTax = acqVal * ACQ_TAX_RT_FST;
+		acqRt = ACQ_TAX_RT_FST * 100;
+		
+	} else {
+		acqTax = acqVal * ACQ_TAX_RT_GEN;
+		acqRt = ACQ_TAX_RT_GEN * 100;
 	}
 	
+	farmTaxAmt = acqTax * FARM_TAX_RT;
+	
+	totTaxAmt = acqTax + farmTaxAmt;
+	dispAcqTax(acqVal, acqTax, farmTaxAmt, totTaxAmt, acqRt);
+	
+}
+
+function dispAcqTax(acqVal, acqTax, farmTaxAmt, totTaxAmt, acqRt){
+	//취득세표시
+	//엘리먼트 추가
+	var obj = document.getElementById("divCalcAcqRst");
+	obj.removeChild(obj.childNodes[0]);
+	
+	var newDIV = document.createElement("div");	
+	newDIV.innerHTML = "<hr><hr>취득세 계산 결과";
+	newDIV.innerHTML += "<p>총취득세 : " + totTaxAmt + "</p>";
+	newDIV.innerHTML += "<p>취득가액 : " + acqVal + "</p>";
+	newDIV.innerHTML += "<p>취득세 : " + acqTax + "</p>";
+	newDIV.innerHTML += "<p>취득세율 : " + acqRt + "%</p>";
+	newDIV.innerHTML += "<p>지방교육세 : " + farmTaxAmt + "</p>";
+	newDIV.setAttribute("id", "divCalcAcqDtl");    
+    newDIV.innerHTML += "<hr><hr>";    
+    obj.appendChild(newDIV);
+    
 }
