36edf43bdf8efdaa53b741874aaba1a8dcb4cfc8
ParkKyungwon <46917458+Park-Kyungwon@users.noreply.github.com>
2019-01-28 09:58:42 +0900

    Add files via upload

--- js/basicCalc.js
+++ js/basicCalc.js
@@ -4,54 +4,57 @@ var temp = 0;
 
 var pointFlag = false;
 
-function btnNumber(num) {
+function btnNumber(panelId, num) {
+	var selId = "#" + panelId;
 	if (!pointFlag) {
 		if (Number(view) == 0 && num == 0) {
-
-		} else if (Number(view) == 0 && num != 0) {
+			
+		} else if (Number(view) == 0 && num != 0) {			
 			view = num;
-		} else if (Number(view) != 0) {
+			
+		} else if (Number(view) != 0) {			
 			view = view + "" + num;
-		}
-
-		$("#message").text(view);
+			
+		}		
+		$(selId).text(view);
 
 	} else {
-		view = view + "" + num;
+		view = view + "" + num;		
+		$(selId).text(view);
 		
-		$("#message").text(view);
 	}
 
 }
 
-function btnClear()	{
+function btnClear(panelId) {
+	var selId = "#" + panelId;
 	view = 0;
-	$("#message").text(0);
+	$(selId).text(0);
 
 }
 
-function btnPlus(){
+function btnPlus(panelId){	
 	temp = view;
 	flag = 1;
 	view = 0;		
 	pointFlag = false;
 }
 
-function btnMinus(){
+function btnMinus(panelId){	
 	temp = view;
 	flag = 2;
 	view = 0;		
 	pointFlag = false;
 }
 
-function btnMulti(){
+function btnMulti(panelId){	
 	temp = view;
 	flag = 3;
 	view = 0;		
 	pointFlag = false;
 }
 
-function btnDivide(){
+function btnDivide(panelId){	
 	temp = view;
 	flag = 4;
 	view = 0;
@@ -59,7 +62,8 @@ function btnDivide(){
 
 }
 
-function btnEqual() {
+function btnEqual(panelId) {
+	var selId = "#" + panelId;
 	if (flag == 1) {
 		temp = Number(temp) + Number(view);
 	} else if (flag == 2) {
@@ -70,20 +74,22 @@ function btnEqual() {
 		temp = Number(temp) / Number(view);
 	}
 
-	$("#message").text(temp);
+	$(selId).text(temp);
 
 	pointFlag = false;
 	view = 0;		
 
 }
 
-function btnBackspace() {
+function btnBackspace(panelId) {
+	var selId = "#" + panelId;
 	view = view.substring(0, view.length-1);
-	$("#message").text(view);	
+	$(selId).text(view);	
 }
 
-function btnPoint() {
+function btnPoint(panelId) {
+	var selId = "#" + panelId;
 	pointFlag = true;
 	view += ".";
-	$("#message").text(view);
+	$(selId).text(view);
 }
--- js/calcYando.js
+++ js/calcYando.js
@@ -84,27 +84,63 @@ function dispAcqTax(acqVal, acqTax, farmTaxAmt, totTaxAmt, acqRt){
 	//취득세표시
 	//엘리먼트 추가	
 	var obj = document.getElementById("divCalcAcqRst");
-	if(obj.hasChildNodes()){
+	while(obj.hasChildNodes()){
 		obj.removeChild(obj.childNodes[0]);
 	}
 		
 	var newDIV = document.createElement("div");
 	newDIV.setAttribute("id", "divCalcAcqDtl");
-	newDIV.innerHTML = "<hr><hr>취득세 계산 결과";
+	newDIV.setAttribute("class", "ui-grid-c calcResult");	
+	newDIV.innerHTML = "<hr><hr>";
+	newDIV.innerHTML += "<p>취득세 계산 결과</p>";
+//	newDIV.innerHTML += "<div class=ui-grid-c'>";
+	newDIV.innerHTML += "<div class='ui-block-a'>총취득세</div>";
+	newDIV.innerHTML += "<div class='ui-block-b' style='text-align:right'>" + $.number(totTaxAmt) + "</div>";
+	newDIV.innerHTML += "<div class='ui-block-c' style='text-align:left'>원</div>";
+	newDIV.innerHTML += "<div class='ui-block-d'></div>";
+	
+	newDIV.innerHTML += "<div class='ui-block-a'>취득가액</div>";
+	newDIV.innerHTML += "<div class='ui-block-b' style='text-align:right'>" + $.number(acqVal) + "</div>";
+	newDIV.innerHTML += "<div class='ui-block-c' style='text-align:left'>원</div>";
+	newDIV.innerHTML += "<div class='ui-block-d'></div>";
+	
+	newDIV.innerHTML += "<div class='ui-block-a'>취득세</div>";
+	newDIV.innerHTML += "<div class='ui-block-b' style='text-align:right'>" + $.number(acqTax) + "</div>";
+	newDIV.innerHTML += "<div class='ui-block-c' style='text-align:left'>원</div>";
+	newDIV.innerHTML += "<div class='ui-block-d'></div>";
+	
+	newDIV.innerHTML += "<div class='ui-block-a'>취득세율</div>";
+	newDIV.innerHTML += "<div class='ui-block-b' style='text-align:right'>" + $.number(acqRt) + "</div>";
+	newDIV.innerHTML += "<div class='ui-block-c' style='text-align:left'>%</div>";
+	newDIV.innerHTML += "<div class='ui-block-d'></div>";
+	
+	newDIV.innerHTML += "<div class='ui-block-a'>지방교육세</div>";
+	newDIV.innerHTML += "<div class='ui-block-b' style='text-align:right'>" + $.number(farmTaxAmt) + "</div>";
+	newDIV.innerHTML += "<div class='ui-block-c' style='text-align:left'>원</div>";
+	newDIV.innerHTML += "<div class='ui-block-d'></div>";	
+	
+	/*
 	newDIV.innerHTML += "<p>총취득세 : " + $.number(totTaxAmt) + "</p>";
 	newDIV.innerHTML += "<p>취득가액 : " + $.number(acqVal) + "</p>";
 	newDIV.innerHTML += "<p>취득세 : " + $.number(acqTax) + "</p>";
 	newDIV.innerHTML += "<p>취득세율 : " + $.number(acqRt) + "%</p>";
-	newDIV.innerHTML += "<p>지방교육세 : " + $.number(farmTaxAmt) + "</p>";	
-    newDIV.innerHTML += "<hr><hr>";    
+	newDIV.innerHTML += "<p>지방교육세 : " + $.number(farmTaxAmt) + "</p>";
+	*/	
+//    newDIV.innerHTML += "<hr><hr>";    
     obj.appendChild(newDIV);
+    
+    var newDIVHr = document.createElement("div");
+    newDIVHr.setAttribute("id", "divHr");
+    newDIVHr.innerHTML = "<hr><hr>";
+    
+    obj.appendChild(newDIVHr);
     
 }
 
 function initAcqTax(){	
 	$("#acqTaxAcqVal").val("");
 	var obj = document.getElementById("divCalcAcqRst");
-	if(obj.hasChildNodes()){
+	while(obj.hasChildNodes()){
 		obj.removeChild(obj.childNodes[0]);
 	}
 }
